2a222cb82aa3d236bd011840bfd219e307114e17
Anton <ton-An@outlook.de>
2023-11-23 20:49:12 +0100

    Removed id property from [User]

--- lib/domain/models/user.dart
+++ lib/domain/models/user.dart
@@ -1,18 +1,14 @@
 /// {@template user}
 /// Base class for all users
 ///
-/// It includes an [id], [username], and [passwordHash]
+/// It includes an [username] and [passwordHash]
 /// {@endtemplate}
 abstract class User {
   /// {@macro user}
   const User({
-    required this.id,
     required this.username,
     required this.passwordHash,
   });
-
-  /// [id] is the unique identifier for the user
-  final String id;
 
   /// [username] is the name that the user will be known by
   final String username;
