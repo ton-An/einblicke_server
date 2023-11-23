e3e9c1de1bd78259baa8b15314b2c85efc0219f8
Anton <ton-An@outlook.de>
2023-11-23 20:53:27 +0100

    Add @JsonSerializable annotation to [PictureFrame]

--- lib/domain/models/pictureFrame.dart
+++ lib/domain/models/pictureFrame.dart
@@ -1,5 +1,8 @@
 import 'package:dispatch_pi_dart/domain/models/curator.dart';
 import 'package:dispatch_pi_dart/domain/models/user.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
 
 /// {@template pictureFrame}
 /// A [PictureFrame] is a frame that displays images sent by [Curator]s
