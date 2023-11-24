e4a672e6b642a015484d7e47327f6b061d22a20a
Anton <ton-An@outlook.de>
2023-11-24 12:47:59 +0100

    Added [FailureStrings] For strings shared between [Failure]s

---
+++ lib/core/failures/failure_strings.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:dispatch_pi_dart/core/failures/failure.dart';
+
+/// {@template failure_strings}
+/// A collection of strings shared between [Failure]s
+/// {@endtemplate}
+class FailureStrings {
+  static const String authenticationCategoryCode = "authentication";
+}
