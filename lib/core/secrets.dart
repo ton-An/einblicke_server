5e20ec295967a108295a02c96ab59f915f82deec
Anton <ton-An@outlook.de>
2023-11-26 21:28:10 +0100

    Added token lifetimes to [Secrets]

--- lib/core/secrets.dart
+++ lib/core/secrets.dart
@@ -5,9 +5,18 @@
 /// Preferably the file name should be secrets_impl.dart
 /// {@endtemplate}
 abstract class Secrets {
+  /// {@macro secrets}
+  const Secrets();
+
   /// The secret used to authenticate the client
   String get clientSecret;
 
   /// The client id used to authenticate the client
   String get clientId;
+
+  /// The lifetime of the access token
+  int get accessTokenLifetime;
+
+  /// The lifetime of the refresh token
+  int get refreshTokenLifetime;
 }
