6e731834ec11dabed31b523ff87d71afd682ffa9
Anton <ton-An@outlook.de>
2023-11-27 17:08:38 +0100

    Add [SignInPictureFrame] usecase

---
+++ lib/domain/uscases/sign_in_picture_frame.dart
@@ -0,0 +1,44 @@
+import 'package:dartz/dartz.dart';
+import 'package:dispatch_pi_dart/core/failures/database_read_failure.dart';
+import 'package:dispatch_pi_dart/core/failures/failure.dart';
+import 'package:dispatch_pi_dart/core/failures/user_not_found_failure.dart';
+import 'package:dispatch_pi_dart/domain/models/authentication_credentials.dart';
+import 'package:dispatch_pi_dart/domain/models/picture_frame.dart';
+import 'package:dispatch_pi_dart/domain/repositories/frame_authentication_repository.dart';
+import 'package:dispatch_pi_dart/domain/uscases/sign_in_wrapper.dart';
+
+/// {@template sign_in_picture_frame}
+/// Use case for signing in a [PictureFrame].
+///
+/// Parameters:
+/// - [String] the picture frame's username
+/// - [String] the picture frame's password
+///
+/// Returns:
+/// - [AuthenticationCredentials] if the picture frame was successfully signed in
+///
+/// Failures:
+/// - [UserNotFoundFailure] if the picture frame was not found
+/// - [DatabaseReadFailure] if the database could not be read
+/// {@endtemplate}
+class SignInPictureFrame {
+  /// {@macro sign_in_picture_frame}
+  SignInPictureFrame({
+    required this.signInPictureFrame,
+  });
+
+  /// The [SignInWrapper] for [PictureFrame]s.
+  final SignInWrapper<PictureFrame, FrameAuthenticationRepository>
+      signInPictureFrame;
+
+  /// {@macro sign_in_picture_frame}
+  Future<Either<Failure, AuthenticationCredentials>> call(
+    String username,
+    String password,
+  ) {
+    return signInPictureFrame(
+      username: username,
+      password: password,
+    );
+  }
+}
---
+++ test/lib/domain/usecases/sign_in_picture_frame_test.dart
@@ -0,0 +1,60 @@
+import 'package:dartz/dartz.dart';
+import 'package:dispatch_pi_dart/core/failures/user_not_found_failure.dart';
+import 'package:dispatch_pi_dart/domain/models/picture_frame.dart';
+import 'package:dispatch_pi_dart/domain/repositories/frame_authentication_repository.dart';
+import 'package:dispatch_pi_dart/domain/uscases/sign_in_picture_frame.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../../../fixtures.dart';
+import '../../../mocks.dart';
+
+void main() {
+  late SignInPictureFrame signInPictureFrame;
+  late MockSignInWrapper<PictureFrame, FrameAuthenticationRepository>
+      mockSignInPictureFrame;
+
+  setUp(() {
+    mockSignInPictureFrame = MockSignInWrapper();
+    signInPictureFrame = SignInPictureFrame(
+      signInPictureFrame: mockSignInPictureFrame,
+    );
+
+    when(
+      () => mockSignInPictureFrame(
+        username: any(named: "username"),
+        password: any(named: "password"),
+      ),
+    ).thenAnswer((_) async => const Right(tAuthenticationCredentials));
+  });
+
+  test(
+      "should call [MockSignInWrapper] and return the [AuthenticationCredentials]",
+      () async {
+    // arrange
+
+    // act
+    final result = await signInPictureFrame(tUsername, tPassword);
+
+    // assert
+    expect(result, const Right(tAuthenticationCredentials));
+    verify(
+        () => mockSignInPictureFrame(username: tUsername, password: tPassword));
+  });
+
+  test("should relay [Failure]s", () async {
+    // arrange
+    when(
+      () => mockSignInPictureFrame(
+        username: any(named: "username"),
+        password: any(named: "password"),
+      ),
+    ).thenAnswer((_) async => const Left(UserNotFoundFailure()));
+
+    // act
+    final result = await signInPictureFrame(tUsername, tPassword);
+
+    // assert
+    expect(result, const Left(UserNotFoundFailure()));
+  });
+}
