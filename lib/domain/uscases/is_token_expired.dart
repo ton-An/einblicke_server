5b720523e6b122b6e7a04f330e8076991e221588
Anton <ton-An@outlook.de>
2023-12-01 19:32:32 +0100

    Added unimplemented [IsTokenExpired] usecase

---
+++ lib/domain/uscases/is_token_expired.dart
@@ -0,0 +1,20 @@
+/// {@template is_token_expired}
+/// A wrapper for checking if a token is expired
+///
+/// Parameters:
+/// - [int] issuedAt in Unix time
+/// - [int] expiresIn in seconds
+///
+/// Returns:
+/// - [bool] indicating if the token is expired
+/// {@endtemplate}
+class IsTokenExpired {
+  /// {@macro is_token_expired}
+  const IsTokenExpired();
+
+  /// {@macro is_token_expired}
+  bool call(int issuedAt, int expiresIn) {
+    // TODO: implement check for expired token
+    throw UnimplementedError();
+  }
+}
