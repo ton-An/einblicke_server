b8d28fa4f554f7f3561675f6beb470816e45403f
Anton <ton-An@outlook.de>
2023-12-03 18:33:58 +0100

    Added [GetNewTokens] usecase

--- test/mocks.dart
+++ test/mocks.dart
@@ -8,8 +8,11 @@ import 'package:dispatch_pi_dart/domain/uscases/create_user/create_user_wrapper.
 import 'package:dispatch_pi_dart/domain/uscases/is_password_valid.dart';
 import 'package:dispatch_pi_dart/domain/uscases/is_username_valid.dart';
 import 'package:dispatch_pi_dart/domain/uscases/sign_in/sign_in_wrapper.dart';
+import 'package:dispatch_pi_dart/domain/uscases/tokens/check_refresh_token_validity/check_refresh_token_validity.dart';
 import 'package:dispatch_pi_dart/domain/uscases/tokens/generate_encrypted_token/generate_access_token.dart';
 import 'package:dispatch_pi_dart/domain/uscases/tokens/generate_encrypted_token/generate_refresh_token.dart';
+import 'package:dispatch_pi_dart/domain/uscases/tokens/invalidate_all_refresh_tokens/invalidate_all_refresh_tokens.dart';
+import 'package:dispatch_pi_dart/domain/uscases/tokens/invalidate_refresh_tokens/invalidate_refresh_token.dart';
 import 'package:dispatch_pi_dart/domain/uscases/tokens/is_token_expired.dart';
 import 'package:dispatch_pi_dart/domain/uscases/tokens/save_refresh_token/save_refresh_token.dart';
 import 'package:mocktail/mocktail.dart';
@@ -49,3 +52,15 @@ class MockSaveRefreshToken<U extends User,
     implements SaveRefreshToken<U, R> {}
 
 class MockEncryptedToken extends Mock implements EncryptedToken {}
+
+class MockCheckRefreshTokenValidityWrapper<U extends User,
+        R extends UserAuthenticationRepository<U>> extends Mock
+    implements CheckRefreshTokenValidityWrapper<U, R> {}
+
+class MockInvalidateRefreshToken<U extends User,
+        R extends UserAuthenticationRepository<U>> extends Mock
+    implements InvalidateRefreshToken<U, R> {}
+
+class MockInvalidateAllRefreshTokens<U extends User,
+        R extends UserAuthenticationRepository<U>> extends Mock
+    implements InvalidateAllRefreshTokens<U, R> {}
