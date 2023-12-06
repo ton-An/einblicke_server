cd12aa0d1e7ad666fb32e5154d3a6fcadbff5681
Anton <ton-An@outlook.de>
2023-12-06 20:07:47 +0100

    Fixed types of [CheckCuratorAccessTokenValidity]

--- lib/features/authentication/domain/uscases/tokens/check_access_token_validity/check_curator_access_token_validity.dart
+++ lib/features/authentication/domain/uscases/tokens/check_access_token_validity/check_curator_access_token_validity.dart
@@ -1,16 +1,16 @@
-import 'package:dispatch_pi_dart/features/authentication/domain/models/picture_frame.dart';
-import 'package:dispatch_pi_dart/features/authentication/domain/repositories/frame_authentication_repository.dart';
+import 'package:dispatch_pi_dart/features/authentication/domain/models/curator.dart';
+import 'package:dispatch_pi_dart/features/authentication/domain/repositories/curator_authentication_repository.dart';
 import 'package:dispatch_pi_dart/features/authentication/domain/uscases/tokens/check_access_token_validity/check_access_token_validity.dart';
 
 /// {@macro check_access_token_validity_wrapper}
 class CheckCuratorAccessTokenValidity extends CheckAccessTokenValidityWrapper<
-    PictureFrame, FrameAuthenticationRepository> {
+    Curator, CuratorAuthenticationRepository> {
   /// {@macro check_access_token_validity_wrapper}
   const CheckCuratorAccessTokenValidity({
     required super.basicAuthRepository,
     required super.isTokenExpiredUseCase,
-    required FrameAuthenticationRepository frameAuthenticationRepository,
+    required CuratorAuthenticationRepository curatorAuthenticationRepository,
   }) : super(
-          userAuthenticationRepository: frameAuthenticationRepository,
+          userAuthenticationRepository: curatorAuthenticationRepository,
         );
 }
