48489fc997ff4eefd2fed43dba7544e7ec08a1a1
Anton <ton-An@outlook.de>
2023-12-06 13:20:04 +0100

    Added [GetImageById] usecase

---
+++ lib/features/image_exchange/domain/usecases/get_image_by_id.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:dispatch_pi_dart/core/failures/cloud_storage_unavailable_failure.dart';
+import 'package:dispatch_pi_dart/core/failures/failure.dart';
+import 'package:dispatch_pi_dart/core/failures/storage_read_failure.dart';
+import 'package:dispatch_pi_dart/features/image_exchange/domain/models/image.dart';
+import 'package:dispatch_pi_dart/features/image_exchange/domain/repositories/image_exchange_repository.dart';
+
+/// {@template get_image_by_id}
+/// Gets an image from storage
+///
+/// Properties:
+/// - [String] imageId
+///
+/// Returns:
+/// - [Image]
+///
+/// Failures:
+/// - [CloudStorageReadFailure]
+/// - [CloudStorageUnavailableFailure]
+/// {@endtemplate}
+class GetImageFromId {
+  /// {@macro get_image_by_id}
+  const GetImageFromId({required this.imageExchangeRepository});
+
+  /// Used for getting the image from storage
+  final ImageExchangeRepository imageExchangeRepository;
+
+  /// {@macro get_image_by_id}
+  Future<Either<Failure, Image>> call({required String imageId}) {
+    return imageExchangeRepository.getImageById(imageId: imageId);
+  }
+}
---
+++ test/lib/features/image_exchange/domain/usecases/get_image_by_id_test.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: inference_failure_on_instance_creation
+
+import 'package:dartz/dartz.dart';
+import 'package:dispatch_pi_dart/core/failures/storage_read_failure.dart';
+import 'package:dispatch_pi_dart/features/image_exchange/domain/usecases/get_image_by_id.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../../../../../fixtures.dart';
+import '../../../../../mocks.dart';
+
+void main() {
+  late GetImageFromId getImageFromId;
+  late MockImageExchangeRepository mockImageExchangeRepository;
+
+  setUp(() {
+    mockImageExchangeRepository = MockImageExchangeRepository();
+    getImageFromId = GetImageFromId(
+      imageExchangeRepository: mockImageExchangeRepository,
+    );
+
+    when(
+      () => mockImageExchangeRepository.getImageById(
+        imageId: any(named: "imageId"),
+      ),
+    ).thenAnswer((_) async => const Right(tImage));
+  });
+
+  test("should get an [Image] from storage and return it", () async {
+    // act
+    final result = await getImageFromId(imageId: tImageId);
+
+    // assert
+    expect(result, const Right(tImage));
+    verify(
+      () => mockImageExchangeRepository.getImageById(
+        imageId: tImageId,
+      ),
+    );
+  });
+
+  test("should relay [Failure]s", () async {
+    // arrange
+    when(
+      () => mockImageExchangeRepository.getImageById(
+        imageId: any(named: "imageId"),
+      ),
+    ).thenAnswer((_) async => const Left(CloudStorageReadFailure()));
+
+    // act
+    final result = await getImageFromId(imageId: tImageId);
+
+    // assert
+    expect(result, const Left(CloudStorageReadFailure()));
+  });
+}
