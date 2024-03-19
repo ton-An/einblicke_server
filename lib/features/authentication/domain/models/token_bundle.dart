90cff31988826ed3a31cfe4af9f9838e80158f5a
Anton <ton-An@outlook.de>
2024-03-19 18:18:57 +0100

    Add JSON serialization to [TokenBundle]

--- lib/features/authentication/domain/models/token_bundle.dart
+++ lib/features/authentication/domain/models/token_bundle.dart
@@ -1,5 +1,10 @@
 import 'package:einblicke_server/features/authentication/domain/models/encrypted_token.dart';
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'token_bundle.g.dart';
+
+@JsonSerializable()
 
 /// {@template authentication_credentials}
 /// __TokenBundle__ is a container for the authentication credentials of a user.
@@ -20,11 +25,12 @@ class TokenBundle extends Equatable {
   /// The refresh token for a user used to generate a new access token
   final EncryptedToken refreshToken;
 
-  /// Converts this instance to a json [Map]
-  Map<String, dynamic> toJson() => {
-        'access_token': accessToken.token,
-        'refresh_token': refreshToken.token,
-      };
+  /// Converts a JSON [Map] into a [TokenBundle] object
+  factory TokenBundle.fromJson(Map<String, dynamic> json) =>
+      _$TokenBundleFromJson(json);
+
+  /// Converts a [TokenBundle] object into a JSON [Map]
+  Map<String, dynamic> toJson() => _$TokenBundleToJson(this);
 
   @override
   List<Object?> get props => [accessToken, refreshToken];
