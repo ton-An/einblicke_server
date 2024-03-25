10d870be45725f246ece5f319693f0aac8ce9d81
Anton <ton-An@outlook.de>
2024-03-25 17:32:00 +0100

    Added route for getting a curators paired frames

--- routes/curator/paired_frames/index.dart
+++ routes/curator/paired_frames/index.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+
 import 'package:dart_frog/dart_frog.dart';
+import 'package:dartz/dartz.dart';
 import 'package:einblicke_server/core/presentation/handlers/failure_response_handler.dart';
+import 'package:einblicke_server/features/authentication/domain/models/curator.dart';
+import 'package:einblicke_server/features/image_exchange/domain/models/paired_frame_info.dart';
+import 'package:einblicke_server/features/image_exchange/domain/usecases/get_paired_frames_info.dart';
+import 'package:einblicke_server/injection_container.dart';
 import 'package:einblicke_shared/einblicke_shared.dart';
 
 Future<Response> onRequest(RequestContext context) async {
@@ -8,6 +15,22 @@ Future<Response> onRequest(RequestContext context) async {
       const MethodNotAllowedFailure(),
     );
   }
-  throw UnimplementedError();
-  // should return a list of paired frames. The list should return the frame id, name and the most recent image
+
+  final Curator curator = context.read<Curator>();
+  final GetPairedFramesInfo getPairedFramesInfo = getIt<GetPairedFramesInfo>();
+
+  final Either<Failure, List<PairedFrameInfo>> pairedFramesInfoEither =
+      await getPairedFramesInfo(curatorId: curator.userId);
+
+  return pairedFramesInfoEither.fold(FailureResponseHandler.getFailureResponse,
+      (List<PairedFrameInfo> pairedFramesInfo) {
+    final List<Map<String, dynamic>> pairedFramesInfoJson =
+        pairedFramesInfo.map((PairedFrameInfo pairedFrameInfo) {
+      return pairedFrameInfo.toJson();
+    }).toList();
+
+    return Response(
+      body: jsonEncode(pairedFramesInfoJson),
+    );
+  });
 }
